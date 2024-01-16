96b4f03c4d9a888bea89b3f751b1b7b0ad7aeb0f
adrianosuprine <adrianosuprine@gmail.com>
2024-01-16 05:34:45 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const newArray = [];
+  for (num of array){
+    result=target-num;
+    if (newArray.includes(result)){
+      return true ;}
+    newArray.push(num);
+  }
+  return false ;
 }
+// console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
 /* 
   Write the Big O time complexity of your function here
+  O(n) because it iterates through the loop (n)being the length of the array
+  
 */
 
 /* 
   Add your pseudocode here
+  start 
+    create an empty array stores it in a variable
+    iterate through the array passed in 
+    for each number in the array checks if (target-sum)is in the array
+    if yes it returns true 
+    if no it adds the number to the new array and returns false if the loop completes
+  stop
+
 */
 
 /*
